bff517e51108dde7908f31072e0cc13ef2942bcc
Lucas Oliver <lucas@oliver.adm.br>
2024-06-19 20:47:29 -0300

    Added functions to terminal class.
    Now terminal has fixed size, is able to scroll and saves up to 64 inputs.
    
    You can now read serial messages via the Terminal class.

--- browser/js/terminal.js
+++ browser/js/terminal.js
@@ -4,7 +4,11 @@ serial_setup_str = `\x03`;
 
 var Terminal = {
   input: document.querySelector('#cmd'),
+  last_inputs: [],
+  selected_input: 0,
   output: document.querySelector('.output'),
+  lines: 0,
+  MAX_LINES: 64,
 
   addLine: function (text) {
     let new_line = document.createElement('div');
@@ -12,26 +16,76 @@ var Terminal = {
     let text_node = document.createTextNode(text);
     new_line.appendChild(text_node);
     this.output.appendChild(new_line);
+    this.output.scrollTop = this.output.scrollHeight;
+    this.selected_input = this.last_inputs.length;
+    if (++this.lines > this.MAX_LINES) {
+      this.output.removeChild(this.output.firstChild);
+      --this.lines;
+    }
     return new_line;
   },
 
   addLines: function (lines) {
     lines.forEach(this.addLine);
+  },
+
+  readFromSerial: async function (ser) {
+    let text = await ser.read();
+    // console.log(text);
+    this.addLine(text);
+    return text;
+  },
+
+  addInput: function (line) {
+    this.last_inputs.push(line);
+    if (this.last_inputs.length > this.MAX_LINES)
+      this.last_inputs.shift();
+  },
+
+  clear: function () {
+    this.output.innerHTML = "";
   }
 };
 
 Terminal.input.addEventListener('keydown', async (event) => {
-  if (event.key == 'Enter') {
-    let line = event.target.value;
-    // console.log(line);
-    await serial.write(line + '\r');
-    event.target.value = '';
+  switch (event.key) {
+    case 'Enter':
+      let line = event.target.value;
+      // console.log(line);
+      Terminal.addInput(line);
+      await serial.write(line + '\r');
+      event.target.value = '';
+      break;
+    case 'ArrowUp':
+      if (--Terminal.selected_input < 0) {
+        ++Terminal.selected_input;
+        break;
+      }
+      event.target.value = Terminal.last_inputs[Terminal.selected_input];
+      break;
+    case 'ArrowDown':
+      if (++Terminal.selected_input > Terminal.last_inputs.length) {
+        --Terminal.selected_input;
+        break;
+      }
+      event.target.value = (Terminal.selected_input >= Terminal.last_inputs.length)?(''):(Terminal.last_inputs[Terminal.selected_input]);
+      break;
   }
+});
+
+document.getElementById("clear").addEventListener("click", () => {
+  Terminal.clear();
 });
 
 setInterval(async () => {
   if (!serialIsConnected) return;
-  let line = await serial.read();
-  // console.log(line);
-  Terminal.addLine(line);
+  let text;
+  try {
+    text = await Terminal.readFromSerial(serial);
+  } catch (e) {
+    if (e === DOMException)
+      serial.disconnect();
+  }
+  if (text.length > 0)
+    console.log(text);
 }, 50);
