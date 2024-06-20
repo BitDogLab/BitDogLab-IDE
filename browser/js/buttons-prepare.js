b0b865ebb494326f1508027f61c38557f4f0ec01
Lucas Oliver <lucas@oliver.adm.br>
2024-06-19 21:18:07 -0300

    Removed unnecessary file, added contents do buttons.js.

--- browser/js/buttons-prepare.js
+++
@@ -1,4 +0,0 @@
-serial_setup_str = `\x03\r` +
-`with open('demos/buttons.py', 'r') as f:\r` +
-`exec(f.read())\r` +
-`\b\r`;
--- browser/js/buttons.js
+++ browser/js/buttons.js
@@ -1,5 +1,10 @@
 'use strict';
 
+serial_setup_str = `\x03\r` +
+`with open('demos/buttons.py', 'r') as f:\r` +
+`exec(f.read())\r` +
+`\b\r`;
+
 let ButtonA_Options = document.querySelector('#button-a-select');
 let buttona_last_value = ButtonA_Options.value;
 let ButtonA_Pages = {
